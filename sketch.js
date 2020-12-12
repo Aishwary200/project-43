e341fc8a1d8549a7659edaf111b53591f12a2f95
Aishwary200 <amritakeshri3059@gmail.com>
2020-12-12 12:29:30 +0530

    project 43

--- sketch.js
+++ sketch.js
@@ -11,35 +11,40 @@ function draw() {
   hr = hour();
   mn = minute();
   sc = second();
-  //console.log(hr+": "+mn+": "+sc)
-  
-  scAngle= map(sc,0,60,0,360)
-  hrAngle= map(hr % 12,0,12,0,180)
-  mnAngle= map(mn,0,60,0,360)
-  push();
-  translate(400,200);
-  rotate(scAngle);
-  stroke(255,0,0);
-  arc(0,0,300,300,0,scAngle);
-  strokeWeight(7);
-  line(0,0,150,0);
-  pop();
-  push();
-  translate(400,200);
-  rotate(mnAngle-90);
+  console.log(hr + ": " + mn + ": " + sc)
+
+  scAngle = map(sc, 0, 60, 0, 360)
+  hrAngle = map(hr % 12, 0, 12, 0, 180)
+  mnAngle = map(mn, 0, 60, 0, 360)
+  stroke(255, 0, 0);
+  strokeWeight(7)
+  arc(400, 200, 300, 300, 0, scAngle);
   stroke("blue");
-  arc(0,0,280,280,0,mnAngle);
+  arc(400, 200, 280, 280, 0, mnAngle);
+  stroke("lime");
+  arc(400, 200, 260, 260, 0, hrAngle);
+  push();
+  translate(400, 200);
+  rotate(scAngle-90);
+  stroke(255, 0, 0);
   strokeWeight(7);
-  line(0,0,100,0);
+  line(0, 0, 100, 0);
   pop();
   push();
-  translate(400,200);
-  rotate(hrAngle);
-  stroke("lime");
-  arc(0,0,260,260,0,hrAngle);
+  translate(400, 200);
+  rotate(mnAngle - 90);
+  stroke("blue");
   strokeWeight(7);
-  line(0,0,80,0);
+  line(0, 0, 75, 0);
+  pop();
+  push();
+  translate(400, 200);
+  rotate(hrAngle - 90);
+  stroke("lime");
+  strokeWeight(7);
+  line(0, 0, 50, 0);
   pop();
   
   drawSprites();
+
 }
